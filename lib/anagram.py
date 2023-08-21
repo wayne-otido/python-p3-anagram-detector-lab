bfadb025bafc0bfe11d93d158ee7c589e4810d64
Wayne Otido <otidowayne@gmail.com>
2023-08-21 22:25:40 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        for word in words:
+            if sorted(word.lower()) == sorted(self.word.lower()) and word.lower() != self.word.lower():
+                anagrams.append(word)
+        return anagrams
